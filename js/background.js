574c5d42ff01a99fb2bc766c88c565b3a7727e66
eattato <80892781+eattato@users.noreply.github.com>
2022-12-22 15:04:58 +0900

    통신 되는 걸로 바꿈

--- js/background.js
+++ js/background.js
@@ -1,12 +1,38 @@
-chrome.extension.onConnect.addListener(function (port) {
-  console.log("popup.js 연결됨");
-  port.onMessage.addListener(function (msg) {
-    port.postMessage("Hi Popup.js");
-    console.log("message sent");
-  });
+let downloadDatas = [];
 
-  // 다운로드 시작 시
-  chrome.downloads.onCreated.addListener((item) => {
-    console.log(item.filename + " is downloading from " + item.finalUrl);
+// popup.js와 통신
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.act == "opened") {
+    // 팝업 오픈 시 현재 다운로드 데이터 전송
+    chrome.runtime.sendMessage({
+      act: "data",
+      param: downloadDatas,
+    });
+  }
+});
+
+// 다운로드 시작 시
+chrome.downloads.onCreated.addListener((item) => {
+  let data = {
+    name: item.filename,
+    path: item.finalUrl,
+    progress: [],
+  };
+
+  let splitCount = 6;
+  let totalSize = item.fileSize;
+  for (let ind = 1; ind <= 6; ind++) {
+    let progressData = {
+      current: 0,
+      max: totalSize / splitCount,
+    };
+    data.progress.push(progressData);
+  }
+  downloadDatas.push(data);
+  console.log(item.filename + " is downloading from " + item.finalUrl);
+
+  chrome.runtime.sendMessage({
+    act: "data",
+    param: downloadDatas,
   });
 });
--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,48 @@
-var port = chrome.extension.connect({
-  name: "splitDL",
-});
-port.postMessage("Hi BackGround");
-port.onMessage.addListener(function (msg) {
-  console.log("message recieved" + msg);
-});
-
+// popup이 열렸을때
 window.addEventListener("load", function () {
-  console.log("popup loaded");
   let urlInput = $(".input_url");
   let dlButton = $(".input_download");
   let dlList = $(".download_list");
 
+  const applyData = (datas) => {
+    dlList.empty();
+    for (let ind in datas) {
+      let data = datas[ind];
+
+      let fileElement = $(".download.origin").clone();
+      fileElement.removeClass("origin");
+      fileElement.find(".download_name").text(data.name);
+      fileElement.find(".download_path").text(data.path);
+      let progressList = fileElement.find(".download_progress");
+
+      for (let ind in data.progress) {
+        let thread = data.progress[ind];
+
+        let threadElement = $(".download_subprogress.origin").clone();
+        threadElement.removeClass("origin");
+        let threadStatus = threadElement.find(".download_bar");
+
+        threadStatus.css({
+          width: Math.floor((thread.current / thread.max) * 100) + "%",
+        });
+        progressList.append(threadElement);
+      }
+    }
+  };
+
+  // background.js와 통신
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.act == "data") {
+      applyData(msg.param);
+    }
+  });
+
+  // background.js에서 현재 정보 받기
+  let data = chrome.runtime.sendMessage({
+    act: "opened",
+  });
+
   dlButton.click(() => {
     let input = urlInput.val();
   });
-})();
+});
